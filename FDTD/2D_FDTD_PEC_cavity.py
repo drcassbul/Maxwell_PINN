304fcd7904f2eb84359e7eaff6b4a9de2675099a
Roman Gelly <gelly.roman@gmail.com>
2024-02-20 17:46:11 +0100

    sdfsd

--- FDTD/2D_FDTD_PEC_cavity.py
+++ FDTD/2D_FDTD_PEC_cavity.py
@@ -9,6 +9,7 @@ Created on Mon Jul 25 09:49:05 2022
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.axes_grid1 import ImageGrid
 from matplotlib.ticker import StrMethodFormatter
 import scipy.constants as c
 from numpy import cos, sin, sqrt
@@ -49,8 +50,8 @@ LX = 1
 LY = 1
 
 # Number of grid points
-NX = 151
-NY = 151
+NX = 51
+NY = 51
 
 # Space increments
 DX = LX / (NX - 1)
@@ -61,7 +62,7 @@ DT = 1 / sqrt(1/DX**2 + 1/DY**2)
 
 # Total integration time
 Tp = sqrt(2) # Time period
-T = 10 * Tp
+T = 1 * Tp
 NT = int(T/DT)
 
 # Initialize spatial grid in physical units
@@ -87,9 +88,9 @@ _, H_X, _ = PEC_cavity(MESH_HX_X, MESH_HX_Y, 0)
 _, _, H_Y = PEC_cavity(MESH_HY_X, MESH_HY_Y, 0)
 
 
-fig = plt.figure()
-ax = axes3d.Axes3D(fig)
-ax.plot_wireframe(MESH_X, MESH_Y, E_Z, rstride=2, cstride=2)
+# fig = plt.figure()
+# ax = axes3d.Axes3D(fig)
+# ax.plot_wireframe(MESH_X, MESH_Y, E_Z, rstride=2, cstride=2)
 
 # =============================================================================
 # Yee's algorithm
@@ -135,7 +136,7 @@ for n in range(NT):
     H_X_ref_array[n] = H_X_ref
     H_Y_ref_array[n] = H_Y_ref
     
-    if n % 1000000 == 0:
+    if n % 100 == 0:
         # fig = plt.figure()
         # ax = axes3d.Axes3D(fig)
         # ax.plot_wireframe(MESH_X, MESH_Y, E_Z_ref, rstride=1, cstride=1)
@@ -150,132 +151,207 @@ for n in range(NT):
         # plt.savefig(f"2D_FDTD_snapshots/num_snap_{n}.png")       
         # plt.close()
         
-        plt.figure()
-        plt.pcolormesh(MESH_X, MESH_Y, E_Z_ref, vmin=-1, vmax=1, cmap='seismic')
-        plt.axis('scaled')
-        plt.colorbar()
-        plt.title(f'$t={(n+1)*DT}$')
-        plt.savefig(f"2D_FDTD_snapshots/snapshots_color/exact_snap_{n}.png")       
-        plt.close()
+        # plt.figure()
+        # plt.pcolormesh(MESH_X, MESH_Y, E_Z_ref, vmin=-1, vmax=1, cmap='seismic')
+        # plt.axis('scaled')
+        # plt.colorbar()
+        # plt.title(f'$t = {n*DT:.3f}$')
+        # plt.tight_layout()
+        # plt.savefig(f"FDTD_cavity/exact_snap_{n}.png")       
+        # plt.close()
     
         plt.figure()
-        plt.pcolormesh(MESH_X, MESH_Y, E_Z, vmin=-1, vmax=1, cmap='seismic')
+        plt.pcolormesh(MESH_X, MESH_Y, E_Z, vmin=-1, vmax=1)
         plt.axis('scaled')
         plt.colorbar()
-        plt.savefig(f"2D_FDTD_snapshots/snapshots_color/num_snap_{n}.png")       
+        plt.title(f'$t = {n*DT:.3f}$')
+        plt.tight_layout()
+        plt.savefig(f"FDTD_cavity/snapshots/num_snap_{n}.png")       
         plt.close()
 
-
-#%%
-
 # =============================================================================
-# Plot temporal evolution of the analytical and numerical solution (Ez, Hx, Hy)
+# Plot field maps at t = 0 and t = T/4
 # =============================================================================
-t_grid = np.linspace(0, T/Tp, NT)
 
-fig, ax = plt.subplots(3, 1, figsize=(10, 8))
+# t = 0
 
-ax[0].plot(t_grid, E_Z_ref_array[:, NX//2, NY//2], 'k-', label='exact')
-ax[0].plot(t_grid, E_Z_array[:, NX//2, NY//2], 'r--', label=f"Yee ($\Delta x = {{{DX:.2f}}}$)")
-ax[0].set_title('$E_z(x=0.5, y=0.5) (t)$')
+fig = plt.figure(figsize=(12, 4.3))
 
-ax[1].plot(t_grid, H_X_ref_array[:, NX//2, 0], 'k-', label='exact')
-ax[1].plot(t_grid, H_X_array[:, NX//2, 0], 'r--', label=f"Yee ($\Delta x = {{{DX:.2f}}}$)")
-ax[1].set_title('$H_x(x=0.5, y=0) (t)$')
+grid = ImageGrid(fig, 111,          # as in plt.subplot(111)
+                 nrows_ncols=(1,3),
+                 axes_pad=0.4,
+                 share_all=True,
+                 cbar_location="right",
+                 cbar_mode="single",
+                 cbar_size="7%",
+                 cbar_pad=0.15,
+                 )
 
-ax[2].plot(t_grid, H_Y_ref_array[:, 0, NX//2], 'k-', label='exact')
-ax[2].plot(t_grid, H_Y_array[:, 0, NX//2], 'r--', label=f"Yee ($\Delta x = {{{DX:.2f}}}$)")
-ax[2].set_title('$H_y(x=0, y=0.5) (t)$')
+# Add data to image grid
+grid[0].pcolormesh(MESH_HX_X, MESH_HX_Y, H_X_array[0], vmin=-1, vmax=1)
+grid[0].set_title(r'$H_x$')
+grid[0].set_ylabel(r'$y$')
+grid[1].pcolormesh(MESH_HY_X, MESH_HY_Y, H_Y_array[0], vmin=-1, vmax=1)
+grid[1].set_title(r'$H_y$')
+im = grid[2].pcolormesh(MESH_X, MESH_Y, E_Z_array[0], vmin=-1, vmax=1)
+grid[2].set_title(r'$E_z$')
 
-ax[2].set_xlabel(r'Time (period)')
-plt.suptitle(r'$N_\textrm{{cells}} = {} \Rightarrow \Delta t = {:.3f}$'.format(NX-1, DT))
+for ax in grid:
+    ax.axis('scaled')
+    ax.set_xlabel(r'$x$')
+
+# Colorbar
+ax.cax.colorbar(im)
+ax.cax.toggle_label(True)
+plt.suptitle('$t=0$')
 plt.tight_layout()
+plt.savefig(f'FDTD_cavity/PEC_field_map_t=0.png')
 
 
-#plt.legend(loc = 'upper left')
-plt.savefig(f'PEC_cavity_figures/comp_exact_num_Nx={NX}.png')
+# Plots at t = T/4
+t0 = Tp / 4
+n0 = int(t0/DT)
+fig = plt.figure(figsize=(12, 4.3))
 
-#%%
-t_grid = np.linspace(0, T/Tp, NT)
+grid = ImageGrid(fig, 111,          # as in plt.subplot(111)
+                 nrows_ncols=(1,3),
+                 axes_pad=0.4,
+                 share_all=True,
+                 cbar_location="right",
+                 cbar_mode="single",
+                 cbar_size="7%",
+                 cbar_pad=0.15,
+                 )
 
-fig, ax = plt.subplots(1, 1, figsize=(10, 2.8))
+# Add data to image grid
+grid[0].pcolormesh(MESH_HX_X, MESH_HX_Y, H_X_array[n0], vmin=-1, vmax=1)
+grid[0].set_title(r'$H_x$')
+grid[0].set_ylabel(r'$y$')
+grid[1].pcolormesh(MESH_HY_X, MESH_HY_Y, H_Y_array[n0], vmin=-1, vmax=1)
+grid[1].set_title(r'$H_y$')
+im = grid[2].pcolormesh(MESH_X, MESH_Y, E_Z_array[n0], vmin=-1, vmax=1)
+grid[2].set_title(r'$E_z$')
 
-ax.plot(t_grid, E_Z_ref_array[:, NX//2, NY//2], 'k-', label='exact')
-ax.plot(t_grid, E_Z_array[:, NX//2, NY//2], 'r--', label=f"Yee ($\Delta x = {{{DX:.2f}}}$)")
-ax.set_title(r'$N_\textrm{{cells}} = {} \Rightarrow \Delta t = {:.3f}$'.format(NX-1, DT))
+for ax in grid:
+    ax.axis('scaled')
+    ax.set_xlabel(r'$x$')
 
-ax.set_xlabel(r'Time (period)')
-
+# Colorbar
+ax.cax.colorbar(im)
+ax.cax.toggle_label(True)
+plt.suptitle('$t=T/4$')
 plt.tight_layout()
+plt.savefig(f'FDTD_cavity/PEC_field_map_t=T\4.png')
+# #%%
 
-#plt.legend(loc = 'upper left')
-plt.savefig(f'PEC_cavity_figures/comp_exact_num_Ez_Nx={NX}.png')
+# # =============================================================================
+# # Plot temporal evolution of the analytical and numerical solution (Ez, Hx, Hy)
+# # =============================================================================
+# t_grid = np.linspace(0, T/Tp, NT)
 
-#%%
-t_grid = np.linspace(0, T/Tp, NT)
-E_num = (np.sum(E_Z_array**2, axis=(1,2)) + np.sum(H_X_array**2, axis=(1,2)) + np.sum(H_Y_array**2, axis=(1,2)))/NX**2
+# fig, ax = plt.subplots(3, 1, figsize=(10, 8))
 
-# =============================================================================
-# Save energy(t)
-# =============================================================================
-np.savetxt(f'energy/energy_NX={NX}.txt', np.stack((t_grid, E_num), axis=1))
-#%%
-# =============================================================================
-# Temporal evolution of energy
-# =============================================================================
+# ax[0].plot(t_grid, E_Z_ref_array[:, NX//2, NY//2], 'k-', label='exact')
+# ax[0].plot(t_grid, E_Z_array[:, NX//2, NY//2], 'r--', label=f"Yee ($\Delta x = {{{DX:.2f}}}$)")
+# ax[0].set_title('$E_z(x=0.5, y=0.5) (t)$')
 
-t_grid = np.linspace(0, T/Tp, NT)
-E_num = (np.sum(E_Z_array**2, axis=(1,2)) + np.sum(H_X_array**2, axis=(1,2)) + np.sum(H_Y_array**2, axis=(1,2)))/NX**2
-E_ref = (np.sum(E_Z_ref_array**2, axis=(1,2)) + np.sum(H_X_ref_array**2, axis=(1,2)) + np.sum(H_Y_ref_array**2, axis=(1,2)))/NX**2
+# ax[1].plot(t_grid, H_X_ref_array[:, NX//2, 0], 'k-', label='exact')
+# ax[1].plot(t_grid, H_X_array[:, NX//2, 0], 'r--', label=f"Yee ($\Delta x = {{{DX:.2f}}}$)")
+# ax[1].set_title('$H_x(x=0.5, y=0) (t)$')
 
-plt.figure()
-plt.plot(t_grid, E_num, 'k-', label='num.')
-plt.hlines(0.25, t_grid.min(), t_grid.max(), colors='k', linestyles='dashed')
-#plt.plot(t_grid, E_ref, label='exact')
-plt.legend()
+# ax[2].plot(t_grid, H_Y_ref_array[:, 0, NX//2], 'k-', label='exact')
+# ax[2].plot(t_grid, H_Y_array[:, 0, NX//2], 'r--', label=f"Yee ($\Delta x = {{{DX:.2f}}}$)")
+# ax[2].set_title('$H_y(x=0, y=0.5) (t)$')
 
-NX_list = [51, 101, 151]
-plt.figure()
+# ax[2].set_xlabel(r'Time (period)')
+# plt.suptitle(r'$N_\textrm{{cells}} = {} \Rightarrow \Delta t = {:.3f}$'.format(NX-1, DT))
+# plt.tight_layout()
 
-plt.hlines(0.25, t_grid.min(), t_grid.max(), colors='k', linestyles='dashed', label='theo.')
-for NX in NX_list:
-    data = np.loadtxt(f'energy/energy_NX={NX}.txt')
-    t_grid = data[:, 0]
-    E_num = data[:, 1]
+
+# #plt.legend(loc = 'upper left')
+# plt.savefig(f'PEC_cavity_figures/comp_exact_num_Nx={NX}.png')
+
+# #%%
+# t_grid = np.linspace(0, T/Tp, NT)
+
+# fig, ax = plt.subplots(1, 1, figsize=(10, 2.8))
+
+# ax.plot(t_grid, E_Z_ref_array[:, NX//2, NY//2], 'k-', label='exact')
+# ax.plot(t_grid, E_Z_array[:, NX//2, NY//2], 'r--', label=f"Yee ($\Delta x = {{{DX:.2f}}}$)")
+# ax.set_title(r'$N_\textrm{{cells}} = {} \Rightarrow \Delta t = {:.3f}$'.format(NX-1, DT))
+
+# ax.set_xlabel(r'Time (period)')
+
+# plt.tight_layout()
+
+# #plt.legend(loc = 'upper left')
+# plt.savefig(f'PEC_cavity_figures/comp_exact_num_Ez_Nx={NX}.png')
+
+# #%%
+# t_grid = np.linspace(0, T/Tp, NT)
+# E_num = (np.sum(E_Z_array**2, axis=(1,2)) + np.sum(H_X_array**2, axis=(1,2)) + np.sum(H_Y_array**2, axis=(1,2)))/NX**2
+
+# # =============================================================================
+# # Save energy(t)
+# # =============================================================================
+# np.savetxt(f'energy/energy_NX={NX}.txt', np.stack((t_grid, E_num), axis=1))
+# #%%
+# # =============================================================================
+# # Temporal evolution of energy
+# # =============================================================================
+
+# t_grid = np.linspace(0, T/Tp, NT)
+# E_num = (np.sum(E_Z_array**2, axis=(1,2)) + np.sum(H_X_array**2, axis=(1,2)) + np.sum(H_Y_array**2, axis=(1,2)))/NX**2
+# E_ref = (np.sum(E_Z_ref_array**2, axis=(1,2)) + np.sum(H_X_ref_array**2, axis=(1,2)) + np.sum(H_Y_ref_array**2, axis=(1,2)))/NX**2
+
+# plt.figure()
+# plt.plot(t_grid, E_num, 'k-', label='num.')
+# plt.hlines(0.25, t_grid.min(), t_grid.max(), colors='k', linestyles='dashed')
+# #plt.plot(t_grid, E_ref, label='exact')
+# plt.legend()
+
+# NX_list = [51, 101, 151]
+# plt.figure()
+
+# plt.hlines(0.25, t_grid.min(), t_grid.max(), colors='k', linestyles='dashed', label='theo.')
+# for NX in NX_list:
+#     data = np.loadtxt(f'energy/energy_NX={NX}.txt')
+#     t_grid = data[:, 0]
+#     E_num = data[:, 1]
     
-    plt.semilogy(t_grid, E_num, label=f'$N_x = {NX}$')
+#     plt.semilogy(t_grid, E_num, label=f'$N_x = {NX}$')
 
 
-plt.xlabel('Time (period)')
-plt.ylabel('Total energy')
-plt.xlim(0,5)
-plt.legend()
-plt.tight_layout()
-plt.savefig(f'energy/energy(t).png')
-#%%
-# =============================================================================
-# Plot temporal evolution of euclidian error (L2 error)
-# =============================================================================
+# plt.xlabel('Time (period)')
+# plt.ylabel('Total energy')
+# plt.xlim(0,5)
+# plt.legend()
+# plt.tight_layout()
+# plt.savefig(f'energy/energy(t).png')
+# #%%
+# # =============================================================================
+# # Plot temporal evolution of euclidian error (L2 error)
+# # =============================================================================
 
-t_grid = np.linspace(0, T/Tp, NT)
+# t_grid = np.linspace(0, T/Tp, NT)
 
-err = 1/((sqrt(NX)*sqrt(NY))) * (np.sum((E_Z_array - E_Z_ref_array)**2, axis=(1,2)) + np.sum((H_X_array - H_X_ref_array)**2, axis=(1,2)) + np.sum((H_Y_array - H_Y_ref_array)**2, axis=(1,2)))**(0.5) 
-np.savetxt(f'L2_error/L2_NX={NX}.txt', np.stack((t_grid, err), axis=1))
-plt.figure()
-plt.loglog(t_grid, err)
-#%%
-NX_list = [51, 101, 151]
-plt.figure()
-for NX in NX_list:
-    data = np.loadtxt(f'L2_error/L2_NX={NX}.txt')
-    t_grid = data[:, 0]
-    err = data[:, 1]
+# err = 1/((sqrt(NX)*sqrt(NY))) * (np.sum((E_Z_array - E_Z_ref_array)**2, axis=(1,2)) + np.sum((H_X_array - H_X_ref_array)**2, axis=(1,2)) + np.sum((H_Y_array - H_Y_ref_array)**2, axis=(1,2)))**(0.5) 
+# np.savetxt(f'L2_error/L2_NX={NX}.txt', np.stack((t_grid, err), axis=1))
+# plt.figure()
+# plt.loglog(t_grid, err)
+# #%%
+# NX_list = [51, 101, 151]
+# plt.figure()
+# for NX in NX_list:
+#     data = np.loadtxt(f'L2_error/L2_NX={NX}.txt')
+#     t_grid = data[:, 0]
+#     err = data[:, 1]
     
-    plt.semilogy(t_grid, err, label=f'$N_x = {NX}$')
+#     plt.semilogy(t_grid, err, label=f'$N_x = {NX}$')
 
-plt.grid()
-plt.legend()
-plt.xlabel('Time (period)')
-plt.ylabel('$L^2$ error')
-plt.tight_layout()
-plt.savefig(f'L2_error/L2_error(t).png')
+# plt.grid()
+# plt.legend()
+# plt.xlabel('Time (period)')
+# plt.ylabel('$L^2$ error')
+# plt.tight_layout()
+# plt.savefig(f'L2_error/L2_error(t).png')
